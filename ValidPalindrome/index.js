d8a3cd0fc9aa1321d8713521c9a377f24d926bfc
Tripti Chhetri <triptichhetri312@gmail.com>
2022-08-04 19:24:35 -0500

    Valid Palindrome Solution

---
+++ ValidPalindrome/index.js
@@ -0,0 +1,40 @@
+/* 
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
+and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise. 
+
+*/
+
+const isPalindrome = (s) => {
+    
+    if(s.length === 0) return true;
+    
+    s = s.toLowerCase();
+    s = s.replace(/[^a-zA-Z0-9]/g, '');
+    
+    let i = 0;
+    let j = s.length -1;    
+    let half = 0;
+    
+    let len = s.length;    
+    if(len % 2 == 0){
+        half = len/2;
+    } else {
+        half = parseInt(len/2) + 1;
+    }
+    
+    while(i < half){
+        
+        if(s[i] !== s[j]) {
+            return false;
+        }
+        
+        i++;
+        j--;
+    }
+    
+    return true;
+    
+};
