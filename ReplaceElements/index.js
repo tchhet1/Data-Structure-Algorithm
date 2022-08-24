6286a5acfac5f932e7db5c6a7ba77e88f86a9cc3
Tripti Chhetri <triptichhetri312@gmail.com>
2022-08-24 00:07:23 -0500

    Replace Elements Solution

---
+++ ReplaceElements/index.js
@@ -0,0 +1,17 @@
+/* 
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1] 
+*/
+
+//loop through the array elements
+//if conditiong to check if greater element exists
+const replaceElements = (arr) => {
+    let maxElement = Math.max(...arr);
+    let result = [];
+
+    for (let index = 0; index < arr.length; index++) {
+        const element = array[index];
+        if(element > ) 
+    }
+}
